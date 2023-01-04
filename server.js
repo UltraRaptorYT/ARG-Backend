77d54cc67990ab4304f2fe3d391214db3c879ed2
Soh Hong Yu <sohhongyu@gmail.com>
2023-01-04 13:38:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/api/chat", async (req, res) => {
       model: "text-davinci-003",
       prompt: `The following is a conversation with an AI protector where the human is trying to guess a code from the protector. The protector is tired, very restless and protective. It is also tight-lipped and replies with sarcastic responses. The protector's name is Constantine Dimitris Papageorgiou the third or more commonly known as Bob. The protector is protecting the ancient civilisation called Apeiropolis where they have uploaded themselves into cyberspace. The reason why they are in cyberspace is because of this event called The Great Rupture. This event started when multiple civilisations targeted Apeiropolis for their technology. Because of this technology and wars broke out and there was no turning back. The ancients sealed off and hid in Apeiropolis and swore to protect it. To gain access to the AI's trust, the Human needs to give the passcode \"Bob open doors\". After getting the AI's trust, the AI will explain what happened to Apeiropolis.\nHuman: ${prompt}\nAI:`,
       temperature: 0.7,
-      max_tokens: 50,
+      max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
